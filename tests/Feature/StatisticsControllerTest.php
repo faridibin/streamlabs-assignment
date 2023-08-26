18385f9f34f62283a1caa0461477394e959903bc
Farid Adam <faridibin@gmail.com>
2023-08-26 12:32:33 -0400

    Create StatisticsControllerTest.php

---
+++ tests/Feature/StatisticsControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Donation;
+use App\Models\Follower;
+use App\Models\MerchSale;
+use App\Models\Subscriber;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class StatisticsControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_can_retrieve_user_statistics()
+    {
+        // Arrange
+        $user = User::factory()->create();
+        Follower::factory(10)->create(['user_id' => $user->id]);
+        Donation::factory(3)->create(['user_id' => $user->id, 'amount' => 50]);
+        Subscriber::factory(3)->create(['user_id' => $user->id, 'subscription_tier' => 'Tier2']);
+        MerchSale::factory(3)->create(['user_id' => $user->id, 'amount' => 30]);
+
+        $response = $this->actingAs($user)->getJson(route('statistics.index'));
+
+        $response->assertStatus(200);
+    }
+}
